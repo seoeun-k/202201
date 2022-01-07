dcbad674e1946aea67fceeaa0487fa880e55cb1a
seoeun-k <70306041+seoeun-k@users.noreply.github.com>
2022-01-07 17:23:33 +0900

    create new project

---
+++ udemy_app/Shared/udemy_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemy_appApp.swift
+//  Shared
+//
+//  Created by seoeun kim on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct udemy_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
